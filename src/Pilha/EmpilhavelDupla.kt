f94091f0bd4873d0dacb1c68f42d9ac0a29b385a
Luiz <luizcaetite@gmail.com>
2024-03-07 14:51:27 -0300

    Interface

---
+++ src/Pilha/EmpilhavelDupla.kt
@@ -0,0 +1,19 @@
+interface EmpilhavelDupla {
+    // Metodos principais -> Pilha1
+    fun empilhar1(dado: Any?)
+    fun desempilhar1(): Any?
+    fun topo1(): Any?
+    // Metodos auxiliares -> Pilha1
+    fun estaCheia1(): Boolean
+    fun estaVazia1(): Boolean
+    fun imprimir1(): String
+
+    // Metodos principais -> Pilha2
+    fun empilhar2(dado: Any?)
+    fun desempilhar2(): Any?
+    fun topo2(): Any?
+    // Metodos auxiliares -> Pilha2
+    fun estaCheia2(): Boolean
+    fun estaVazia2(): Boolean
+    fun imprimir2(): String
+}
