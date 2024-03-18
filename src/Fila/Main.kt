4b379b6f96b3f87fd51d060db8fe29a4bb34ca99
Luiz <luizcaetite@gmail.com>
2024-03-18 15:06:53 -0300

    Main

---
+++ src/Fila/Main.kt
@@ -0,0 +1,34 @@
+fun main() {
+    val fila: DuplamenteEnfileiravel = FilaEstaticaDupla()
+    fila.enfileirarInicio("A")
+    println(fila.imprimirFrentePraTras()) //[A]
+    println(fila.espiarInicio()) //A
+    println(fila.espiarInicio()) //A
+    fila.enfileirarInicio("B")
+    fila.desenfileirarFim()
+    fila.enfileirarFim("c")
+    fila.atualizarFim("C")
+    fila.desenfileirarInicio()
+    fila.enfileirarInicio("D")
+    println(fila.imprimirFrentePraTras()) //[D,C]
+    fila.espiarInicio()
+    fila.desenfileirarFim()
+    println(fila.espiarFim()) //D
+    fila.enfileirarInicio("e")
+    fila.atualizarInicio("E")
+    println(fila.imprimirFrentePraTras())//[E,D]
+    fila.enfileirarFim("F")
+    fila.desenfileirarInicio()
+    fila.enfileirarFim("G")
+    println(fila.espiarFim()) //G
+    fila.enfileirarInicio("H")
+    fila.desenfileirarFim()
+    fila.enfileirarFim("I")
+    println(fila.imprimirFrentePraTras()) //[H,D,F,I]
+    fila.enfileirarInicio("J")
+    fila.enfileirarFim("K")
+    fila.enfileirarFim("L")
+    println(fila.espiarInicio()) //J
+    fila.desenfileirarInicio()
+    println(fila.imprimirFrentePraTras()) //[H,D,F,I,K,L]
+}
