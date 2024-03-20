ae18c785ab2e841cc9e1f2c24344571aa6e7f05b
Luiz <luizcaetite@gmail.com>
2024-03-20 14:50:51 -0300

    Interface Fila com Pilhas

---
+++ src/Fila/Fila Com Pilhas/Empilhavel.kt
@@ -0,0 +1,13 @@
+package Fila.`Fila Com Pilhas`
+
+interface Empilhavel {
+    // Metodos principais
+    fun empilhar(dado: Any?)
+    fun desempilhar(): Any?
+    fun espiar(): Any?
+    fun atualizar(dado: Any?)
+    // Metodos auxiliares
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
---
+++ src/Fila/Fila Com Pilhas/Enfileiravel.kt
@@ -0,0 +1,13 @@
+package Fila.`Fila Com Pilhas`
+
+interface Enfileiravel {
+    // Metodos principais
+    fun enfileirar(dado: Any?)
+    fun desenfileirar(): Any?
+    fun frente(): Any?
+    fun atualizar(dado: Any?)
+    // Metodos auxiliares
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
