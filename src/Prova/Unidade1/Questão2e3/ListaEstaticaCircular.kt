a5a53006d22662567bb4ee6a564ec3e1b7db7cdf
Luiz <luizcaetite@gmail.com>
2024-04-02 13:34:32 -0300

    Método 05 - Buscar dado de uma posição específica em uma Lista

--- src/Prova/Unidade1/Questão2e3/ListaEstaticaCircular.kt
+++ src/Prova/Unidade1/Questão2e3/ListaEstaticaCircular.kt
@@ -12,6 +12,23 @@ class ListaEstaticaCircular(val tamanho: Int = 10) {
         return quantidade == 0
     }
 
+    // 05 - Buscar um dado de uma posição lógica específica em uma Lista Estatica Circular
+    fun Buscar(posicao: Int): Any? {
+        var dadoRetono: Any? = null
+        if (!estaVazia()) {
+            if (posicao >= 0 && posicao < quantidade) {
+                //Mapeamento de posição física
+                var posiçãoFisica = (posicao+ponteiroInicio) % dados.size
+                dadoRetono = dados[posiçãoFisica]
+            } else {
+                println("Invalid Index!")
+            }
+        } else {
+            println("List is Empty!")
+        }
+        return dadoRetono
+    }
+
     // 10 - Atualizar o dado do fim de uma Lista Estatica Circular
     fun atualizarFim(dado: Any?) {
         if (!estaVazia()) {
