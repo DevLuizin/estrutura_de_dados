671b6e9ff039b04fd8a183cd25816ae7f05b4c5b
Luiz <luizcaetite@gmail.com>
2024-04-09 11:06:55 -0300

    Utilização da Classe HeapMaxPaciente (Main)

---
+++ src/Heap/Estatico/Paciente - Arvore Binaria Heap Maximo/HeapPacienteUtil.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var heapPacientes: HeapfiablePaciente = HeapMaxPaciente(10)
+    heapPacientes.inserir(Paciente("João", 30, 3))
+    heapPacientes.inserir(Paciente("Maria", 25, 1))
+    heapPacientes.inserir(Paciente("Pedro", 40, 2))
+    heapPacientes.inserir(Paciente("Ana", 35, 4))
+    println("")
+    println("${heapPacientes.imprimir()}") // Saída: [Paciente(nome=Ana, idade=35, prioridade=4), Paciente(nome=João, idade=30, prioridade=3), Paciente(nome=Pedro, idade=40, prioridade=2), Paciente(nome=Maria, idade=25, prioridade=1)]
+    println("")
+    val pacienteAtendido = heapPacientes.extrair()
+    println("Paciente atendido: $pacienteAtendido") // Saída: Paciente atendido: Paciente(nome=Ana, idade=35, prioridade=4)
+    println("")
+    println("${heapPacientes.imprimir()}") // Saída: [Paciente(nome=João, idade=30, prioridade=3), Paciente(nome=Maria, idade=25, prioridade=1), Paciente(nome=Pedro, idade=40, prioridade=2)]
+}
