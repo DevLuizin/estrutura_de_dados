c298c0c4e34416fd6120311261e6ff0548fcb940
Luiz <luizcaetite@gmail.com>
2024-07-09 14:39:51 -0300

    Hash Versão 2

---
+++ src/Hash/Versao2/HashUtil2.kt
@@ -0,0 +1,71 @@
+package Hash.Versao2
+
+//Universally Unique Identifier (UUID)
+//é uma sequencia de 128 bits usada para
+//identificação em sistemas computacionais
+import java.util.UUID
+import Hash.Versao1.Cliente
+import Hash.Versao1.Espalhavel
+import Hash.Versao1.Mapa
+import Hash.Versao1.MapaEspalhamento
+
+fun main() {
+    //elementos que serão introduzidos na ED
+    val c1 = Cliente("Claudio Rodolfo", 30)
+    val c2 = Cliente("Maria Isabel", 12)
+    val c3 = Cliente("Antonio Carlos", 23)
+    val c4 = Cliente("João Pedro", 21)
+    val c5 = Cliente("Maria Eduarda", 53)
+    val c6 = Cliente("Carlos Eduardo", 56)
+
+    //popula a Estrutura MapaEspalhamento
+    var hashmap: Espalhavel = MapaEspalhamento(2)
+
+    val k1 = UUID.randomUUID()
+    hashmap.adicionar(Mapa(k1, c1))
+
+    val k2 = UUID.randomUUID()
+    hashmap.adicionar(Mapa(k2, c2))
+
+    val k3 = UUID.randomUUID()
+    hashmap.adicionar(Mapa(k3, c3))
+
+    val k4 = UUID.randomUUID()
+    hashmap.adicionar(Mapa(k4, c4))
+
+    val k5 = UUID.randomUUID()
+    hashmap.adicionar(Mapa(k5, c5))
+
+    val k6 = UUID.randomUUID()
+    hashmap.adicionar(Mapa(k6, c6))
+
+    //chave para testes
+    var chave = k1;	//Cliente("Claudio Rodolfo", 30)
+    val clienteTemp = hashmap.buscar(chave) as Cliente
+    println("a idade de $chave é: ${clienteTemp.idade}")
+
+    //verifica se há elemento com a chave k2
+    chave = k2	//Cliente("Maria Isabel", 12)
+    if (hashmap.contemChave(chave))
+        println("contem a chave $chave,\nreferenciando o objeto ${hashmap.buscar(chave)}")
+    else
+        println("nao contem a chave $chave")
+
+    //verifica se há elemento com uma chave qualquer
+    chave = UUID.randomUUID()	//Cliente não existente
+    if (hashmap.contemChave(chave))
+        println("contem a chave $chave,\nreferenciando o objeto ${hashmap.buscar(chave)}")
+    else
+        println("nao contem a chave $chave")
+
+    //remove o elemento referenciado pela chave k2
+    hashmap.remover(k2)
+
+    //verifica se há elemento com a chave k2
+    chave = k2	//Cliente("Maria Isabel", 12)
+    if (hashmap.contemChave(chave))
+        println("contem a chave $chave,\nreferenciando o objeto ${hashmap.buscar(chave)}")
+    else
+        println("não contem a chave $chave")
+
+}
---
+++ src/Hash/Versao2/MapaEspelhamento2.kt
@@ -0,0 +1,144 @@
+package Hash.Versao2
+
+import Hash.Versao1.Espalhavel
+import Hash.Versao1.Listavel
+import Hash.Versao1.ListaDinamica
+import Hash.Versao1.Mapa
+import java.util.UUID
+
+//Clusterização, Conteinerização
+class MapaEspalhamento2(private val tamanhoTabela: Int = 10): Espalhavel {
+
+    private var tabelaEspalhamento: Array<Listavel> = Array(tamanhoTabela) { ListaDinamica() }
+
+    //Controla quantos elementos há na estrutura Mapa Espalhamento (HashMap)
+    private var quantidade = 0
+
+    //funcao que espalha os elementos "chave,valor" para cairem em listas diferentes
+    //baseado na paridade da chave (par, impar)
+    private fun funcaoEspalhamento(chave: Any): Int {
+        val uuid = chave as UUID
+        val ultimosBit = Math.abs(uuid.leastSignificantBits.toInt())
+        return (ultimosBit % 2)
+    }
+
+    override fun adicionar(mapa: Mapa) {
+        val indice = funcaoEspalhamento(mapa.chave)
+        val listaTemp = tabelaEspalhamento[indice]
+        // Se a chave existir, atualiza o mapa;
+        //caso contrário, insere o mapa
+        if (contemChave(mapa.chave)) {
+            for (i in 0 until listaTemp.tamanho()) {
+                val elementoLista = listaTemp.selecionar(i) as Mapa
+                val chaveLista = elementoLista.chave
+
+                if (chaveLista == mapa.chave) {
+                    listaTemp.atualizar(i, mapa)
+                    break
+                }
+            }
+        } else {
+            listaTemp.anexar(mapa)
+            quantidade = quantidade.inc()
+        }
+    }
+
+    override fun remover(chave: Any): Any? {
+        var dadoAuxiliar: Any? = null
+        if (!estaVazia()) {
+            if (contemChave(chave)) {
+                // Obtém o índice da tabela de espalhamento que a chave pertence
+                // Seleciona um item da tabela (esse item é uma lista)
+                val indice = funcaoEspalhamento(chave)
+                val listaTemp = tabelaEspalhamento[indice]
+
+                // Para cada elemento da lista é verificado se a chave informada é igual à chave do elemento
+                for (i in 0 until listaTemp.tamanho()) {
+                    val elementoLista = listaTemp.selecionar(i) as Mapa
+                    val chaveLista = elementoLista.chave
+
+                    if (chave == chaveLista) {
+                        dadoAuxiliar = elementoLista.dado
+                        listaTemp.apagar(i)
+                        quantidade = quantidade.dec()
+                        break
+                    }
+                }
+            } else {
+                println("Chave não existente.")
+            }
+        } else {
+            println("A estrutura de dados está vazia.")
+        }
+        return dadoAuxiliar
+    }
+
+    override fun contemChave(chave: Any): Boolean {
+        var chaveEncontrada = false
+        if (!estaVazia()) {
+            // Obtém o índice da tabela de espalhamento que a chave pertence
+            // Seleciona um item da tabela (esse item é uma lista)
+            val indice = funcaoEspalhamento(chave)
+            val listaTemp = tabelaEspalhamento[indice]
+
+            // Para cada elemento da lista é verificado se a chave informada é igual à chave do elemento
+            for (i in 0 until listaTemp.tamanho()) {
+                val elementoLista = listaTemp.selecionar(i) as Mapa
+                val chaveLista = elementoLista.chave
+
+                if (chave == chaveLista) {
+                    chaveEncontrada = true
+                    break
+                }
+            }
+        } else {
+            println("A estrutura de dados está vazia.")
+        }
+        return chaveEncontrada
+    }
+
+    override fun buscar(chave: Any): Any? {
+        var dadoAuxiliar: Any? = null
+        if (!estaVazia()) {
+            if (contemChave(chave)) {
+                // Obtém o índice da tabela de espalhamento que a chave pertence
+                // Seleciona um item da tabela (esse item é uma lista)
+                val indice = funcaoEspalhamento(chave)
+                val listaTemp = tabelaEspalhamento[indice]
+
+                // Para cada elemento da lista é verificado se a chave informada é igual à chave do elemento
+                for (i in 0 until listaTemp.tamanho()) {
+                    val elementoLista = listaTemp.selecionar(i) as Mapa
+                    val chaveLista = elementoLista.chave
+
+                    if (chave == chaveLista) {
+                        dadoAuxiliar = elementoLista.dado
+                        break
+                    }
+                }
+            } else {
+                println("Chave não existente.")
+            }
+        } else {
+            println("A estrutura de dados está vazia")
+        }
+        return dadoAuxiliar
+    }
+
+    override fun tamanho(): Int {
+        return quantidade
+    }
+
+    override fun estaVazia(): Boolean {
+        return (quantidade == 0)
+    }
+
+    override fun imprimir(): String {
+        var resultado = "["
+        for (i in 0 until tabelaEspalhamento.size) {
+            val listaTemp = tabelaEspalhamento[i]
+            resultado += listaTemp.imprimir()
+        }
+        return "$resultado]"
+    }
+}
