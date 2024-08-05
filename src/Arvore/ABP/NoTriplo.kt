8f7cfe957d09cd56bacffd3d8e9a4de8694f0560
Luiz <luizcaetite@gmail.com>
2024-08-05 17:36:15 -0300

    Data Class NoTriplo

--- src/Arvore/ABP/NoTriplo.kt
+++ src/Arvore/ABP/NoTriplo.kt
@@ -1,5 +1,5 @@
 data class NoTriplo(var dado: Any? = null) {
-    var genitor: NoTriplo? = null
-    var esquerda: NoTriplo? = null
-    var direita: NoTriplo? = null
+	var genitor: NoTriplo? = null
+	var esquerda: NoTriplo? = null
+	var direita: NoTriplo? = null
 }
