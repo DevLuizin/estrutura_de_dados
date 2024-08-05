9f71bb6407ad1d475b098ba5b1fd61e898f5649c
Luiz <luizcaetite@gmail.com>
2024-08-05 18:05:51 -0300

    Data Class NoTriplo -  Árvore AVL

---
+++ src/Arvore/AVL/NoTriplo.kt
@@ -0,0 +1,8 @@
+package AVL
+
+data class NoTriplo<T>(var dado: T? = null)(
+    var genitor: NoTriplo<T>? = null
+    var esquerda: NoTriplo<T>? = null
+    var direita: NoTriplo<T>? = null
+    var altura: Int = 0
+)
